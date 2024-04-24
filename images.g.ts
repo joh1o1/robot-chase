8cb3c22f561d563213a850fe9a0836411aa3a5bc
joh1o1 <156935722+joh1o1@users.noreply.github.com>
2024-04-24 11:46:53 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "robot 1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f f f . . . . . 
+. . . . . . . f b b b f . . . . 
+. . . . . . . f 2 2 6 f . . . . 
+. . . . . f . f 6 6 f . . . . . 
+. . . . . f f b f f b f . . . . 
+. . . . . . f f 4 4 f f . . . . 
+. . . . . . . f f f f f . . . . 
+. . . . . . . f 6 6 f . . . . . 
+. . . . . . . f f f f . . . . . 
+. . . . . . f f . . f f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
